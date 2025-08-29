e2bf0066e9b7680742316e8daea9be14e3859aa4
Dayala212005 <00045824@uca.edu.sv>
2025-08-28 20:21:21 -0600

    Examen 1 Parte Practica

---
+++ script.js
@@ -0,0 +1,37 @@
+
+    function cambiarTitulo() {
+        const titulo = document.getElementById("titulo");
+        titulo.textContent = "Titulo Nuevo";
+    }
+
+    function cambiarLayout() {
+        const contenedor = document.getElementById("contenedor")
+        if (contenedor.style.display === "block") {
+            contenedor.style.display = "grid";
+            contenedor.style.gridTemplateColumns = "25% 70%";
+            contenedor.style.gap = "5%";
+        } else {
+            contenedor.style.display = "block";
+        }
+    }
+
+    function cambiarSubtitulo() {
+        const subtitulo = document.getElementById("subtitulo");
+        subtitulo.style.color = subtitulo.style.color === "red" ? "black" : "red";
+
+        const parrafo = subtitulo.nextElementSibling;
+        parrafo.style.color = parrafo.style.color === "blue" ? "black" : "blue";
+    }
+
+    function agregarImagen() {
+        const aside = document.querySelector("aside");
+        if (!document.getElementById("img-extra")) {
+            const img = document.createElement("img");
+            img.src = "Logo.jpg";
+            img.alt = "Imagen HTML";
+            img.id = "img-extra";
+            img.style.width = "100%";
+            img.style.marginTop = "10px";
+            aside.appendChild(img);
+        }
+    }
